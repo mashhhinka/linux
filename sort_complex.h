1ca96c6954e7dce27aca2b78668ad50654f318eb
mashhhinka <mariaaslanyan96@gmail.com>
2024-10-09 12:30:06 +0400

    Adding ComplexNumber class

---
+++ sort_complex.h
@@ -0,0 +1,11 @@
+// sort_complex.h
+#ifndef SORT_COMPLEX_H
+#define SORT_COMPLEX_H
+
+#include "ComplexNumber.h"
+
+// Function to sort an array of ComplexNumbers based on their absolute value
+void sortComplexNumbers(ComplexNumber arr[], int size);
+
+#endif // SORT_COMPLEX_H
+
