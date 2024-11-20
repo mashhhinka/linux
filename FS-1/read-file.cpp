17ff9592de371dd8a247307abd69375695c14b38
mashhhinka <mariaaslanyan96@gmail.com>
2024-11-20 16:25:18 +0400

    adding FS-1

---
+++ FS-1/read-file.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Error: Missing file path argument" << std::endl;
+        return 1;
+    }
+
+    const char* filePath = argv[1];
+    std::ifstream file(filePath);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open the file " << filePath << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    file.close();
+    return 0;
+}
